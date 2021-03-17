0e40e1f4fcf1ddf61ec1114d18e253e056b4beb8
ollls <ostrygun@gmail.com>
2021-03-17 09:00:36 -0700

    update

--- src/main/scala/server/TLSServer.scala
+++ src/main/scala/server/TLSServer.scala
@@ -30,7 +30,7 @@ class TLSServer[MyEnv <: Has[MyLogging.Service]](
   val KEYSTORE_PASSWORD = keyStorePwd
   val TLS_PROTO         = tlsVersion //default TLSv1.2 in JDK8
   val BINDING_SERVER_IP = serverIP //make sure certificate has that IP on SAN's list
-  val KEEP_ALIVE: Long  = keepAlive //ms, good if short for testing with broken site's snaphosts with 404 pages
+  val KEEP_ALIVE        = keepAlive //ms, good if short for testing with broken site's snaphosts with 404 pages
   val SERVER_PORT       = port
 
   private var processor: Channel => ZIO[ZEnv with MyEnv, Exception, Unit] = null
